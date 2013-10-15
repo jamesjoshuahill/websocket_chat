73d5b00c305c276f292054021486c78c10e66d30
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-15 10:41:08 +0100

    Prepare new rails app with websockets-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RealtimeChat::Application.config.secret_key_base = 'b40a59b73cd7f8ef59214758280d83215b162625f6e4e2d80635eeeb09f95976b03a152ae9cd8eaf7c6ef462f7114e34bb862676692fb5d747aa8cdcfe44d4fb'
