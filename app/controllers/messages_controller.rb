910825df5ddca14a724773185091bbad034cd341
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-16 11:14:15 +0100

    Tidy messages controller and add flash notice for evisceration

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,23 +11,18 @@ class MessagesController < ApplicationController
   # POST /messages
   def create
     @message = Message.new(message_params)
-
-    respond_to do |format|
-      if @message.save
-        format.html do
-          WebsocketRails[:chat].trigger 'new', @message
-          redirect_to root_path, notice: 'Message was posted.'
-        end
-      else
-        render action: 'new'
-      end
+    if @message.save
+      WebsocketRails[:chat].trigger 'new', @message
+      redirect_to root_path, notice: 'Message was posted.'
+    else
+      render action: 'new'
     end
   end
 
   # DELETE /messages/1
   def destroy
     @message.destroy
-    redirect_to root_path
+    redirect_to root_path, notice: 'Message eviscerated.'
   end
 
   private
